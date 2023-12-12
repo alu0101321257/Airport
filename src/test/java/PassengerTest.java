f07e4c02b76388a0661e9978b5a3832360a942c3
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-12 20:59:23 +0000

    Update PassengerTest.java

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,6 +1,4 @@
-package es.ull.passengers;
 import es.ull.flights.Flight;
-
 import es.ull.passengers.Passenger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,12 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class FlightTest {
 
     private Flight flight;
+    private Passenger passenger;
 
     @BeforeEach
     void setUp() {
-        // Initialize a Flight object before each test
+        // Initialize a Flight and Passenger object before each test
         flight = new Flight("AB123", 50);
+        passenger = new Passenger("ID123", "John Doe", "US");
     }
+
+    // Flight class tests
 
     @Test
     void testGetFlightNumber() {
@@ -28,7 +30,6 @@ class FlightTest {
 
     @Test
     void testAddPassenger() {
-        Passenger passenger = new Passenger("ID123", "John Doe", "US");
         assertTrue(flight.addPassenger(passenger));
         assertEquals(1, flight.getNumberOfPassengers());
         assertEquals(flight, passenger.getFlight());
@@ -36,7 +37,6 @@ class FlightTest {
 
     @Test
     void testRemovePassenger() {
-        Passenger passenger = new Passenger("ID123", "John Doe", "US");
         flight.addPassenger(passenger);
         assertTrue(flight.removePassenger(passenger));
         assertEquals(0, flight.getNumberOfPassengers());
@@ -56,4 +56,61 @@ class FlightTest {
         smallFlight.addPassenger(passenger1);
         assertThrows(RuntimeException.class, () -> smallFlight.addPassenger(passenger2));
     }
+
+    // Passenger class tests
+
+    @Test
+    void testGetIdentifier() {
+        assertEquals("ID123", passenger.getIdentifier());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("John Doe", passenger.getName());
+    }
+
+    @Test
+    void testGetCountryCode() {
+        assertEquals("US", passenger.getCountryCode());
+    }
+
+    @Test
+    void testGetFlight() {
+        assertNull(passenger.getFlight());
+    }
+
+    @Test
+    void testJoinFlight() {
+        passenger.joinFlight(flight);
+        assertEquals(flight, passenger.getFlight());
+        assertTrue(flight.getPassengers().contains(passenger));
+    }
+
+    @Test
+    void testJoinFlightWithPreviousFlight() {
+        Flight previousFlight = new Flight("CD456", 30);
+        passenger.joinFlight(previousFlight);
+
+        passenger.joinFlight(flight);
+
+        assertEquals(flight, passenger.getFlight());
+        assertTrue(flight.getPassengers().contains(passenger));
+        assertFalse(previousFlight.getPassengers().contains(passenger));
+    }
+
+    @Test
+    void testJoinFlightWithFailedRemove() {
+        Flight mockFlight = new Flight("EF789", 20);
+        // Make the previous flight unable to remove the passenger
+        passenger.setFlight(mockFlight);
+
+        assertThrows(RuntimeException.class, () -> passenger.joinFlight(flight));
+    }
+
+    @Test
+    void testToString() {
+        String expectedString = "Passenger John Doe with identifier: ID123 from US";
+        assertEquals(expectedString, passenger.toString());
+    }
 }
+
