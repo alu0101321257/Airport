d4ad7acf510b70572cdc158cee84395310cf183a
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-16 18:36:14 +0000

    Update Flight.java

--- src/main/java/es/ull/flights/Flight.java
+++ src/main/java/es/ull/flights/Flight.java
@@ -19,128 +19,99 @@
  * ========================================================================
  */
 
-/**
- * This file contains the implementation of the Passenger class.
+*
+ * ========================================================================
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
  */
-package es.ull.passengers;
-
-import java.util.Arrays;
-import java.util.Locale;
-
-import es.ull.flights.Flight;
 
 /**
- * Represents a passenger with a unique identifier, name, and country code.
+ * @file Flight.java
+ * @brief This file contains the implementation of the Flight class.
  */
-public class Passenger {
+package es.ull.flights;
 
-    /**
-     * Unique identifier for the passenger.
-     */
-    private final String identifier;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    /**
-     * Name of the passenger.
-     */
-    private final String name;
+import es.ull.passengers.Passenger;
+/**
+ * @class Flight
+ * @brief Represents a flight with a unique flight number and a certain number of seats.
+ */
+public class Flight {
 
-    /**
-     * Country code of the passenger.
-     */
-    private final String countryCode;
+    private String flightNumber; ///< Unique identifier for the flight.
+    private int seats; ///< Number of available seats on the flight.
+    private Set<Passenger> passengers = new HashSet<>(); ///< Set of passengers currently booked on the flight.
 
+    private static String flightNumberRegex = "^[A-Z]{2}\\d{3,4}$"; ///< Regular expression for valid flight numbers.
+    private static Pattern pattern = Pattern.compile(flightNumberRegex); ///< Pattern object for flight number validation.
     /**
-     * The flight that the passenger is currently on.
+     * @brief Constructs a Flight object with the specified flight number and number of seats.
+     * @param flightNumber The unique identifier for the flight.
+     * @param seats The number of available seats on the flight.
+     * @throws RuntimeException If the provided flight number is invalid.
      */
-    private Flight flight;
-
-    /**
-     * Constructs a Passenger object with the specified identifier
-     *name, and country code.
-     * @param newIdentifier The unique identifier for the passenger.
-     * @param newName The name of the passenger.
-     * @param newCountryCode The country code of the passenger.
-     * @throws RuntimeException If the provided country code is invalid.
-     */
-    public Passenger(final String newIdentifier,
-                     final String newName, final String newCountryCode) {
-        if (!Arrays.asList(Locale.getISOCountries()).contains(newCountryCode)) {
-            throw new RuntimeException("Invalid country code");
+    public Flight(String flightNumber, int seats) {
+        Matcher matcher = pattern.matcher(flightNumber);
+        if (!matcher.matches()) {
+            throw new RuntimeException("Invalid flight number");
         }
-
-        this.identifier = newIdentifier;
-        this.name = newName;
-        this.countryCode = newCountryCode;
+        this.flightNumber = flightNumber;
+        this.seats = seats;
     }
-
     /**
-     * Gets the identifier of the passenger.
-     * @return The passenger identifier.
+     * @brief Gets the flight number of the flight.
+     * @return The flight number.
      */
-    public String getIdentifier() {
-        return identifier;
+    public String getFlightNumber() {
+        return flightNumber;
     }
-
     /**
-     * Gets the name of the passenger.
-     * @return The passenger name.
+     * @brief Gets the number of passengers currently booked on the flight.
+     * @return The number of passengers.
      */
-    public String getName() {
-        return name;
+    public int getNumberOfPassengers() {
+        return passengers.size();
     }
-
     /**
-     * Gets the country code of the passenger.
-     * @return The country code.
+     * @brief Adds a passenger to the flight.
+     * @param passenger The passenger to be added.
+     * @return True if the passenger was successfully added, false otherwise.
+     * @throws RuntimeException If there are not enough seats for the flight.
      */
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    /**
-     * Gets the flight that the passenger is currently on.
-     * @return The current flight.
-     */
-    public Flight getFlight() {
-        return flight;
-    }
-
-    /**
-     * Joins a new flight, leaving the previous one if applicable.
-     * @param newFlight The new flight to join.
-     * @throws RuntimeException If the passenger cannot be
-     *removed from the previous flight or added to the new one.
-     */
-    public void joinFlight(final Flight newFlight) {
-        Flight previousFlight = this.flight;
-        if (null != previousFlight) {
-            if (!previousFlight.removePassenger(this)) {
-                throw new RuntimeException("Cannot remove passenger");
-            }
+    public boolean addPassenger(Passenger passenger) {
+        if (getNumberOfPassengers() >= seats) {
+            throw new RuntimeException("Not enough seats for flight " + getFlightNumber());
         }
-        setFlight(newFlight);
-        if (null != newFlight) {
-            if (!newFlight.addPassenger(this)) {
-                throw new RuntimeException("Cannot add passenger");
-            }
-        }
+        passenger.setFlight(this);
+        return passengers.add(passenger);
     }
-
     /**
-     * Sets the current flight of the passenger.
-     * @param newFlight The new flight.
+     * @brief Removes a passenger from the flight.
+     * @param passenger The passenger to be removed.
+     * @return True if the passenger was successfully removed, false otherwise.
      */
-    public void setFlight(final Flight newFlight) {
-        this.flight = newFlight;
-    }
-
-    /**
-     * Returns a string representation of the passenger.
-     * @return The string representation.
-     */
-    @Override
-    public String toString() {
-        return "Passenger " + getName() + " with identifier: "
-            + getIdentifier() + " from " + getCountryCode();
+    public boolean removePassenger(Passenger passenger) {
+        passenger.setFlight(null);
+        return passengers.remove(passenger);
     }
 }
