2b7b463511aa0a371fb4b162fc79c41fdda1c18c
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-17 17:24:35 +0000

    Update FlightTest.java (doc)

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,30 +1,44 @@
+/**
+ * @file FlightTest.java
+ * @brief Test cases for the Flight class.
+ */
 package es.ull.flights;
 import es.ull.flights.Flight;
 import es.ull.passengers.Passenger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * @brief Test class for the Flight class.
+ */
 class FlightTest {
 
     private Flight flight;
-
+    /**
+     * @brief Setup method to initialize a Flight object before each test.
+     */
     @BeforeEach
     void setUp() {
         // Initialize a Flight object before each test
         flight = new Flight("AB123", 50);
     }
-
+    /**
+     * @brief Test case for the getFlightNumber method.
+     */
     @Test
     void testGetFlightNumber() {
         assertEquals("AB123", flight.getFlightNumber());
     }
-
+    /**
+     * @brief Test case for the getNumberOfPassengers method.
+     */
     @Test
     void testGetNumberOfPassengers() {
         assertEquals(0, flight.getNumberOfPassengers());
     }
-
+    /**
+     * @brief Test case for the addPassenger method.
+     */
     @Test
     void testAddPassenger() {
         Passenger passenger = new Passenger("ID123", "John Doe", "US");
@@ -32,7 +46,9 @@ class FlightTest {
         assertEquals(1, flight.getNumberOfPassengers());
         assertEquals(flight, passenger.getFlight());
     }
-
+    /**
+     * @brief Test case for the removePassenger method.
+     */
     @Test
     void testRemovePassenger() {
         Passenger passenger = new Passenger("ID123", "John Doe", "US");
@@ -41,12 +57,16 @@ class FlightTest {
         assertEquals(0, flight.getNumberOfPassengers());
         assertNull(passenger.getFlight());
     }
-
+    /**
+     * @brief Test case for creating a Flight with an invalid flight number.
+     */
     @Test
     void testInvalidFlightNumber() {
         assertThrows(RuntimeException.class, () -> new Flight("InvalidNumber", 100));
     }
-
+    /**
+     * @brief Test case for adding a passenger to a flight with not enough seats.
+     */
     @Test
     void testNotEnoughSeats() {
         Flight smallFlight = new Flight("XY456", 1);
